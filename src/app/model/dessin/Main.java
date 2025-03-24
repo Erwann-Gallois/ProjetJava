7b8b1d5a89d2b68b6fd9c583ea9f1828929de6df
Erwann-Gallois <gallois.erwann@gmail.com>
2025-03-24 12:09:42 +0100

    Refactor ShapeButtonPanel layout and add 'Termine' button; comment out printShapes functionality in Main

--- src/app/model/dessin/Main.java
+++ src/app/model/dessin/Main.java
@@ -13,18 +13,18 @@ public class Main {
             frame.setVisible(true);
 
             // Add a button to print the shapes
-            JButton printShapesButton = new JButton("Print Shapes");
-            printShapesButton.addActionListener(e -> {
-                ArrayList<Shape> shapes = frame.getShapes();
-                for (Shape shape : shapes) {
-                    System.out.println("--------------------");
-                    System.out.println("Points: (" + shape.getBounds().x + ", " + shape.getBounds().y + ")");
-                    System.out.println("Width: " + shape.getBounds().width);
-                    System.out.println("Height: " + shape.getBounds().height);
-                    System.out.println("--------------------");
-                }
-            });
-            frame.add(printShapesButton, BorderLayout.SOUTH);
+            // JButton printShapesButton = new JButton("Print Shapes");
+            // printShapesButton.addActionListener(e -> {
+            //     ArrayList<Shape> shapes = frame.getShapes();
+            //     for (Shape shape : shapes) {
+            //         System.out.println("--------------------");
+            //         System.out.println("Points: (" + shape.getBounds().x + ", " + shape.getBounds().y + ")");
+            //         System.out.println("Width: " + shape.getBounds().width);
+            //         System.out.println("Height: " + shape.getBounds().height);
+            //         System.out.println("--------------------");
+            //     }
+            // });
+            // frame.add(printShapesButton, BorderLayout.SOUTH);
 
             // // Print out the shapes
             // for (Shape shape : shapes) {
--- src/app/model/dessin/ShapeButtonPanel.java
+++ src/app/model/dessin/ShapeButtonPanel.java
@@ -1,26 +1,33 @@
 package app.model.dessin;
 
 import javax.swing.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+
 
 public class ShapeButtonPanel extends JPanel implements ActionListener {
     private JButton rectangleButton;
     private JButton circleButton;
+    private JButton termineButton;
     private String currentShape = "rectangle";
     private JLabel shapeLabel;
 
     public ShapeButtonPanel() {
         rectangleButton = new JButton("Rectangle");
         circleButton = new JButton("Circle");
+        termineButton = new JButton("Termine");
         shapeLabel = new JLabel("Selected Shape: Rectangle");
 
         rectangleButton.addActionListener(this);
         circleButton.addActionListener(this);
+        termineButton.addActionListener(this);
 
-        add(rectangleButton);
-        add(circleButton);
-        add(shapeLabel);
+        add(rectangleButton, BorderLayout.WEST);
+        add(circleButton, BorderLayout.CENTER);
+        add(shapeLabel, BorderLayout.CENTER);
+        add(termineButton, BorderLayout.EAST);
     }
 
     @Override
@@ -32,6 +39,7 @@ public class ShapeButtonPanel extends JPanel implements ActionListener {
             currentShape = "circle";
             shapeLabel.setText("Selected Shape: Circle");
         }
+
     }
 
     public String getCurrentShape() {
